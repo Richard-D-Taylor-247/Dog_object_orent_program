e1ffede0ebab3f51c4ff9c048efbb8a2b434159c
Richard Taylor <richard.dewayne.taylor@gmail.com>
2019-03-18 23:36:49 -0500

    adds main files

---
+++ Dog.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Dog.h
+ * Author: richard
+ *
+ * Created on March 18, 2019, 10:41 PM
+ */
+
+#ifndef DOG_H
+#define DOG_H
+
+
+
+#endif /* DOG_H */
+
